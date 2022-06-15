6535ddafd327b5ae5aeeffa27134a223fe28afe0
Pouya Hallaj Zavareh <40316639+Pouyaexe@users.noreply.github.com>
2022-06-15 19:34:39 +0430

    reading jason

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 # an app to convert jason to df
 import json
 import pandas as pd
+
+#convert jason to df
+
+import pandas as pd
+
+# Read json from String
+json_str = '{"Courses":{"r1":"Spark"},"Fee":{"r1":"25000"},"Duration":{"r1":"50 Days"}}'
+
+df = pd.read_json('result.json', orient='records')
+print(df)
